bd886ab775991e83665fddd8322a4101d3c0e38e
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-04-05 10:55:52 -0300

    defining liked-list methods signature

---
+++ data-structures/linked-list/LinkedList.py
@@ -0,0 +1,47 @@
+from Node import Node
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.__start = None
+        self.__end = None
+        self.__num_of_elements = 0
+
+    def is_empty(self) -> bool:
+        pass
+
+    def insert_at_start(self, value) -> None:
+        pass
+
+    def insert_at_end(self, value) -> None:
+        pass
+
+    def insert_after(self, target, value) -> None:
+        pass
+
+    def insert_before(self, target, value) -> None:
+        pass
+
+    def insert(self, index: int, value) -> None:
+        pass
+
+    def remove_first(self):
+        pass
+
+    def remove_last(self):
+        pass
+
+    def remove(self, value):
+        pass
+
+    def remove_at_position(self, index: int):
+        pass
+
+    def get_first(self):
+        pass
+
+    def get_last(self):
+        pass
+
+    def get_at_position(self, index: int):
+        pass
