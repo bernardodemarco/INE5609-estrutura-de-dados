c2427375d65a829562a86f6be1735448078fffd8
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-04-18 15:18:19 -0300

    starting to write DoubleLinkedList Docs

--- data-structures/double-linked-list/DoubleLinkedList.py
+++ data-structures/double-linked-list/DoubleLinkedList.py
@@ -3,6 +3,15 @@ from Node import Node
 
 class DoubleLinkedList:
     def __init__(self, max_length: int) -> None:
+        """
+        The constructor for DoubleLinkedList class.
+
+        Args:
+            max_length (int): maximum length of the list.
+
+        Raises:
+            Exception: if max_length is not a number of if it is less than 1.
+        """
         self.__start = None
         self.__cursor = None
         self.__num_of_elements = 0
@@ -13,27 +22,43 @@ class DoubleLinkedList:
                 'max_length parameter must be an integer greater than zero')
 
     def __go_forward_k_positions(self, k: int) -> None:
+        """
+        Offset cursor k positions forward.
+
+        Args:
+            k (int): number of positions to offset the cursor.
+        """
         counter = 0
         while counter < k:
             self.__cursor = self.__cursor.next
             counter += 1
 
-    def __go_back_k_positions(self, k: int) -> None:
+    def __go_backward_k_positions(self, k: int) -> None:
+        """
+        Offset cursor k positions backwards.
+
+        Args:
+            k (int): number of positions to offset the cursor.
+        """
         counter = 0
         while counter < k:
             self.__cursor = self.__cursor.previous
             counter += 1
 
     def __go_to_start(self) -> None:
+        """Move cursor to the start of the list."""
         self.__cursor = self.__start
 
     def __go_to_end(self) -> None:
+        """Move cursor to then end of the list."""
         self.__cursor = self.__start.previous
 
     def is_empty(self) -> bool:
+        """Return a boolean indicating whether the list is empty or not."""
         return self.__num_of_elements == 0
 
     def is_full(self) -> bool:
+        """Return a boolean indicating whether the list is full or not."""
         return self.__num_of_elements == self.__max_length
 
     def __handle_insertion_with_empty_list(self, value) -> None:
@@ -126,7 +151,7 @@ class DoubleLinkedList:
             self.__start = self.__cursor.next
             self.__go_forward_k_positions(1)
         else:
-            self.__go_back_k_positions(1)
+            self.__go_backward_k_positions(1)
 
         return temp.value
 
@@ -145,6 +170,18 @@ class DoubleLinkedList:
         return self.remove_current()
 
     def remove_at_position(self, index: int):
+        """
+        Remove element at a given position from the list.
+
+        Args:
+            index (int): position of the element to be removed.
+
+        Returns:
+            element that has been removed.
+
+        Raises:
+            Exception: if index is less than one or greater than the number of elements.
+        """
         if index < 1 or index > self.__num_of_elements:
             raise Exception('Index out of range')
 
@@ -153,6 +190,15 @@ class DoubleLinkedList:
         return self.remove_current()
 
     def contains(self, target) -> bool:
+        """
+        Check if a given element is in the list.
+
+        Args:
+            target: element to be searched for in the list.
+
+        Returns:
+            bool: boolean value indicating whether the element is in the list or not.
+        """
         if self.is_empty():
             return False
 
@@ -171,12 +217,25 @@ class DoubleLinkedList:
         return True
 
     def get_current(self):
+        """Return the current element."""
         if self.is_empty():
             raise Exception('List is empty')
 
         return self.__cursor.value
 
     def get_position_of(self, target) -> int:
+        """
+        Get the position of a given element is in the list.
+
+        Args:
+            target: element to be searched for in the list.
+
+        Returns:
+            int: position of the given element in the list.
+
+        Raises:
+            Exception: if list is empty or the element does not exist in the list.
+        """
         if self.is_empty():
             raise Exception('List is empty')
 
@@ -197,21 +256,25 @@ class DoubleLinkedList:
         return position
 
     def get_start(self):
+        """Return the element that is at the start of the list."""
         if self.is_empty():
             raise Exception('List is empty')
 
         return self.__start.value
 
     def get_end(self):
+        """Return the element that is at the end of the list."""
         if self.is_empty():
             raise Exception('List is empty')
 
         return self.__start.previous.value
 
     def get_length(self) -> int:
+        """Return the length of the list."""
         return self.__num_of_elements
 
     def print_list(self) -> None:
+        """Print the list."""
         if self.is_empty():
             print('START - END (EMPTY LIST)')
             return
