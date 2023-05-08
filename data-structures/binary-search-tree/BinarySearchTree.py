297f565ae3257dfe427ad9e4acfe62a415df0b1e
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-05-08 17:33:50 -0300

    remove and search methods

--- data-structures/binary-search-tree/BinarySearchTree.py
+++ data-structures/binary-search-tree/BinarySearchTree.py
@@ -5,6 +5,9 @@ class BinarySearchTree:
     def __init__(self) -> None:
         self.__root = None
 
+    def is_empty(self) -> bool:
+        return self.__root == None
+
     def __insert(self, value, root):
         if self.is_empty():
             self.__root = Node(value)
@@ -18,6 +21,36 @@ class BinarySearchTree:
         else:
             root.right = self.__insert(value, root.right)
         return root
+
+    def __min(self, root):
+        while root.left:
+            root = root.left
+
+        return root
+
+    def __remove(self, key, root):
+        if root == None:
+            return root
+
+        if key < root.value:
+            root.left = self.__remove(key, root.left)
+            return root
+        elif key > root.value:
+            root.right = self.__remove(key, root.right)
+            return root
+        else:
+            if root.left and root.right:
+                new_root = self.__min(root.right)
+                root.value = new_root.value
+                root.right = self.__remove(new_root.value, root.right)
+                return root
+            else:
+                if root.left:
+                    return root.left
+                elif root.right:
+                    return root.right
+                else:
+                    return None
 
     def __pre_order(self, root):
         if root != None:
@@ -37,11 +70,25 @@ class BinarySearchTree:
             print(root.value, end=' ')
             self.__in_order(root.right)
 
-    def is_empty(self) -> bool:
-        return self.__root == None
+    def search(self, key):
+        root = self.__root
+
+        while root != None and root.value != key:
+            if key < root.value:
+                root = root.left
+            else:
+                root = root.right
+
+        if root == None:
+            raise Exception('The given element does not exist in the tree')
+
+        return root.value
 
     def insert(self, value):
         self.__insert(value, self.__root)
+
+    def remove(self, key):
+        self.__remove(key, self.__root)
 
     def print_pre_order(self):
         self.__pre_order(self.__root)
