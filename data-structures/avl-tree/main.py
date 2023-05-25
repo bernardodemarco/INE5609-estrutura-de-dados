b7adc782d9a83254ded51197947423760f951758
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-05-25 15:12:35 -0300

    started to test avl tree algorithms

--- data-structures/avl-tree/main.py
+++ data-structures/avl-tree/main.py
@@ -2,38 +2,52 @@ from AVLTree import AVLTree
 
 
 tree = AVLTree()
-tree.insert(1)
-tree.insert(2)
-tree.insert(3)
-tree.insert(6)
-tree.insert(5)
-tree.insert(4)
-tree.insert(7)
-tree.insert(8)
-tree.insert(4.5)
-tree.insert(4.25)
 
-print('PRE-ORDER SEQUENCE BEFORE DELETIONS')
+nums_to_insert = [39, 29, 85, 31, 55, 91]
+
+for num in nums_to_insert:
+    tree.insert(num)
+
+print('PRE-ORDER SEQUENCE')
 tree.print_pre_order()
 
-tree.remove(8)
-tree.remove(7)
-tree.remove(4)
-tree.remove(6)
-tree.remove(1)
-tree.remove(2)
-tree.remove(5)
-
-print('\n\nPRE-ORDER SEQUENCE AFTER DELETIONS')
-print('REMOVED 8, 7, 4, 6, 1, 2')
+tree.insert(32)
+print('\n\nPRE-ORDER SEQUENCE AFTER LEFT ROTATE')
 tree.print_pre_order()
 
-# nums = [9, 5, 10, 0, 6, 11, -1, 1, 2]
-# for num in nums:
-#     tree.insert(num)
+tree.insert(50)
+tree.insert(45)
+print('\n\nPRE-ORDER SEQUENCE AFTER RIGHT ROTATE')
+tree.print_pre_order()
 
+tree.insert(60)
+print('\n\nPRE-ORDER SEQUENCE AFTER LEFT-RIGHT ROTATE')
+tree.print_pre_order()
+
+tree.insert(95)
+print('\n\nPRE-ORDER SEQUENCE AFTER LEFT ROTATE')
+tree.print_pre_order()
+
+tree.insert(94)
+print('\n\nPRE-ORDER SEQUENCE AFTER RIGHT-LEFT ROTATE')
+tree.print_pre_order()
+
+tree.remove(60)
+print('\n\nPRE-ORDER SEQUENCE AFTER DELETION OF 60 WITH LEFT ROTATE')
+tree.print_pre_order()
+
+# tree.remove(95)
+# print('\n\nPRE-ORDER SEQUENCE AFTER DELETION OF 60 WITH LEFT ROTATE')
 # tree.print_pre_order()
 
-# tree.remove(10)
-# print('')
+# tree.remove(8)
+# tree.remove(7)
+# tree.remove(4)
+# tree.remove(6)
+# tree.remove(1)
+# tree.remove(2)
+# tree.remove(5)
+
+# print('\n\nPRE-ORDER SEQUENCE AFTER DELETIONS')
+# print('REMOVED 8, 7, 4, 6, 1, 2')
 # tree.print_pre_order()
